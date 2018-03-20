4346103f1fa336dda9df34ba6882b4ef64a49c98
Javier Montagna <jmontagna@solstice.com>
2018-03-20 09:47:31 -0300

    Fix typo

--- dockerHubDeploy.sh
+++ dockerHubDeploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$ curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jmontagna/quote-of-the-day/trigger/86ed4dd7-3635-47e2-9182-a636d0a3f5bf/
+curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/jmontagna/quote-of-the-day/trigger/86ed4dd7-3635-47e2-9182-a636d0a3f5bf/
