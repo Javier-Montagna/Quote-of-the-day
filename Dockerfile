7e22d8459328524ebb1ef314cbff16f8e5e81b09
Javier Montagna <jmontagna@solstice.com>
2018-03-20 11:51:58 -0300

    Adjusted file to support local image functionality

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM node:8.9-alpine
 ENV NODE_ENV production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
-RUN npm install --production --silent && mv node_modules ../
+RUN npm install 
+# If you are building your code for production
+# RUN npm install --only=production
 COPY . .
 EXPOSE 8080
-CMD npm start
+CMD [ "npm", "start" ]
---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+/****************************** Router ***************************/
+router.get('*', function(req, res){
+    res.sendFile('index.html', { root: __dirname + '/' });
+});
+
+/****************************** /Router ***************************/
+
+app.use(express.static(__dirname + '/'));
+app.use('/', router);
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
