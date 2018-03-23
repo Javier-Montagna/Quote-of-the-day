d59d2c1dca6c3a6abf84857f8fa3608725e77c49
Javier Montagna <jmontagna@solstice.com>
2018-03-23 15:54:47 -0300

    Finish with the sdk installation and set up of container

--- gcloud-sdk-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-gcloud version || true
-if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
-  
-# Add gcloud to $PATH
-source $HOME/google-cloud-sdk/path.bash.inc
-gcloud version
