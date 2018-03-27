a0b20236a468f000351e2156c7011238b113bac6
Javier Montagna <jmontagna@solstice.com>
2018-03-27 10:56:22 -0300

    Changing color to green

--- create-deploy.sh
+++ create-deploy.sh
@@ -5,5 +5,6 @@ if [ $? -ne 0 ]; then
   kubectl create -f quote-of-the-day-deployment.yaml; 
 else 
   echo "Updating deployment"; 
+  sed -ie "s/SALT_TO_BE_REPLACE_DURING_DEPLOYMENT/$(date)/g" quote-of-the-day-deployment.yaml
   kubectl apply -f quote-of-the-day-deployment.yaml --record; 
 fi
